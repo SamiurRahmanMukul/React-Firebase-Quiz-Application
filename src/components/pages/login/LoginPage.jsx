328649ca50a221ef847118b9b18b86d50850ec9b
Mukul <samiur@siliconorchard.com>
2022-04-03 10:38:34 +0600

    after login & singup redirect to home page

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginImage from "../../../assets/images/login.svg";
+import { useAuth } from "../../../context-api/AuthContext";
 import Illustration from "../singup/illustration/Illustration";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (currentUser) {
+      window.location.href = "/";
+    }
+  }, [currentUser]);
+
   return (
     <>
       <h1>Login to your account</h1>
--- src/components/pages/singup/SingupPage.jsx
+++ src/components/pages/singup/SingupPage.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SingupImage from "../../../assets/images/singup.svg";
+import { useAuth } from "../../../context-api/AuthContext";
 import Illustration from "./illustration/Illustration";
 import SingupForm from "./SingupForm";
 
 export default function SingupPage() {
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (currentUser) {
+      window.location.href = "/";
+    }
+  }, [currentUser]);
+
   return (
     <>
       <h1>Create an account</h1>
